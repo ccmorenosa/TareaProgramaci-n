6f0dea68aebc9a35e3903b8665e72d50b144febe
Andres F. Moreno Sarria <idiotpeople95@gmail.com>
2020-01-23 20:21:09 -0500

    .

---
+++ Ejercicio_2.1/build/CMakeFiles/Solucion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Solucion.dir/Solución.cpp.o"
+  "Solucion.pdb"
+  "Solucion"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Solucion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
